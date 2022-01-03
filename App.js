a3ff32c02a83467a322c611d916c4bab587a04a8
DESKTOP-G34PGPB\Kieu Tran <henry.tran@htmi.ch>
2022-01-02 17:38:08 -0800

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is FoodBook test App</Text>
+      <Button title="Testing" onPress={() => alert('hey')}/>
       <StatusBar style="auto" />
     </View>
   );
